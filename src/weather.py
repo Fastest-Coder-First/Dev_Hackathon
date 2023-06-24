06472d6c0e39398b762cacdd7b8287407887f460
mk-pamulapati <36884631+mk-pamulapati@users.noreply.github.com>
2023-06-24 11:56:22 +0530

    Add files via upload

--- src/weather.py
+++ src/weather.py
@@ -4,83 +4,94 @@ import requests
 
 
 "Api parameters"
-API_KEY = "5e88f2488e93e15b31de9f3b8008dc1d"
+API_KEY = "5e88f2488e93e15b31de9f3b8008dc1d" # Replace with your own API key
 API_URL = "https://api.openweathermap.org/data/2.5/forecast"
 
-def get_weather(city):
-    # Validate input
+
+
+def validate_input(city):
+    """
+    Validates the input city name.
+
+    Args:
+        city (str): The name of the city to validate.
+
+    Raises:
+        ValueError: If the city name is invalid.
+    """
     if not isinstance(city, str):
-        print("City name must be a string")
-        return
+        raise ValueError("City name must be a string")
     if not city.isalpha():
-        print("City name must contain only alphabetical characters")
-        return
+        raise ValueError("City name must contain only alphabetical characters")
     if len(city) < 3:
-        print("City name must be at least 3 characters long")
-        return
+        raise ValueError("City name must be at least 3 characters long")
     if len(city) > 50:
-        print("City name must be no more than 50 characters long")
-        return
-    
-    # Make API request
+        raise ValueError("City name must be no more than 50 characters long")
+
+
+def make_api_request(city):
+    """
+    Sends an API request to OpenWeatherMap and returns the response object.
+
+    Args:
+        city (str): The name of the city to get weather data for.
+
+    Returns:
+        requests.Response: The response object from the API request.
+
+    Raises:
+        ValueError: If an error occurs while making the API request.
+    """
     params = {"q": city, "appid": API_KEY, "units": "metric"}
     try:
         response = requests.get(API_URL, params=params, timeout=5)
         response.raise_for_status()  # raise an exception for 4xx or 5xx status codes
     except requests.exceptions.HTTPError as err:
         if response.status_code == 401:
-            print("Invalid API key or unauthorized access. Please check your API key.")
+            raise ValueError("Invalid API key or unauthorized access. Please check your API key.")
         elif response.status_code == 404:
-            print(f"No weather data found for {city}")
+            raise ValueError(f"No weather data found for {city}")
         else:
-            print(f"HTTP error occurred: {err}")
-        return
+            raise ValueError(f"HTTP error occurred: {err}")
     except requests.exceptions.RequestException as err:
-        print(f"An error occurred: {err}")
-        return
+        raise ValueError(f"An error occurred: {err}")
     except requests.exceptions.ConnectionError as err:
-        print(f"Connection error occurred: {err}")
-        return
+        raise ValueError(f"Connection error occurred: {err}")
     except requests.exceptions.Timeout as err:
-        print(f"Request timed out: {err}")
-        return
-    
-    # Parse response data
+        raise ValueError(f"Request timed out: {err}")
+    return response
+
+
+def get_weather(city):
+    """
+    Gets the current weather and 5-day forecast for a city.
+
+    Args:
+        city (str): The name of the city to get weather data for.
+    """
     try:
+        validate_input(city)
+        response = make_api_request(city)
         data = response.json()
-    except ValueError as err:
-        print(f"Error parsing response: {err}")
-        return
-    try:
         current_temp = data["list"][0]["main"]["temp"]
         current_desc = data["list"][0]["weather"][0]["description"]
-    except (KeyError, IndexError):
-        print(f"No weather data found for {city}")
-        return
-    
-    # Print current weather
-    print(f"\nCurrent weather in {city}:")
-    print(f"┌─────────────┬────────────────┐")
-    print(f"│ Temperature │ {current_temp:.1f}°C │")
-    print(f"├─────────────┼────────────────┤")
-    print(f"│ Description │ {current_desc:<14}│")
-    print(f"└─────────────┴────────────────┘")
-    
-    # Print 5-day forecast
-    print("\n5-day forecast:")
-    print(f"┌─────────────┬─────────────┬────────────────┐")
-    print(f"│ Date        │ Temperature │ Description    │")
-    print(f"├─────────────┼─────────────┼────────────────┤")
-    for forecast in data["list"][1:]:  # skip first item (current weather)
-        try:
+        forecast_data = data["list"][1:]
+        print(f"\nCurrent weather in {city}:")
+        print(f"Temperature: {current_temp:.1f}°C")
+        print(f"Description: {current_desc}")
+        print("\n5-day forecast:")
+        print(f"┌─────────────┬─────────────┬────────────────┐")
+        print(f"│ Date        │ Temperature │ Description    │")
+        print(f"├─────────────┼─────────────┼────────────────┤")
+        for forecast in forecast_data:
             date = forecast["dt_txt"]
             temp = forecast["main"]["temp"]
             desc = forecast["weather"][0]["description"]
-        except (KeyError, IndexError):
-            print(f"Error parsing forecast data for {city}")
-            return
-        print(f"│ {date} │ {temp:.1f}°C │ {desc:<14}│")
-    print(f"└─────────────┴─────────────┴────────────────┘")
+            print(f"│ {date} │ {temp:.1f}°C │ {desc:<14}│")
+        print(f"└─────────────┴─────────────┴────────────────┘")
+    except ValueError as err:
+        print(f"Error: {err}")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Get current weather and 5-day forecast for a city")
--- tests/test_weather.py
+++ tests/test_weather.py
@@ -2,69 +2,85 @@ import unittest
 from unittest.mock import patch
 from weather import get_weather
 
-class TestWeather(unittest.TestCase):
-    @patch("weather.requests.get")
-    def test_get_weather_valid_city(self, mock_get):
-        mock_data = {
+class TestGetWeather(unittest.TestCase):
+    @patch("builtins.print")
+    def test_invalid_input(self, mock_print):
+        # Test invalid input
+        get_weather(123)
+        mock_print.assert_called_with("City name must be a string")
+        get_weather("New York City!")
+        mock_print.assert_called_with("City name must contain only alphabetical characters")
+        get_weather("LA")
+        mock_print.assert_called_with("City name must be at least 3 characters long")
+        get_weather("This is a very long city name that exceeds the maximum length allowed")
+        mock_print.assert_called_with("City name must be no more than 50 characters long")
+    
+    @patch("builtins.print")
+    @patch("requests.get")
+    def test_api_error(self, mock_get, mock_print):
+        # Test API error
+        mock_get.return_value.status_code = 401
+        get_weather("London")
+        mock_print.assert_called_with("Invalid API key or unauthorized access. Please check your API key.")
+        mock_get.return_value.status_code = 404
+        get_weather("Paris")
+        mock_print.assert_called_with("No weather data found for Paris")
+        mock_get.side_effect = requests.exceptions.Timeout
+        get_weather("Tokyo")
+        mock_print.assert_called_with("Request timed out: ")
+    
+    @patch("builtins.print")
+    @patch("requests.get")
+    def test_valid_input(self, mock_get, mock_print):
+        # Test valid input
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {
             "list": [
                 {
-                    "main": {"temp": 20},
-                    "weather": [{"description": "sunny"}]
-                },
-                {
                     "dt_txt": "2022-01-01 12:00:00",
-                    "main": {"temp": 18},
-                    "weather": [{"description": "partly cloudy"}]
+                    "main": {"temp": 10.0},
+                    "weather": [{"description": "clear sky"}],
                 },
                 {
                     "dt_txt": "2022-01-02 12:00:00",
-                    "main": {"temp": 16},
-                    "weather": [{"description": "cloudy"}]
-                }
+                    "main": {"temp": 15.0},
+                    "weather": [{"description": "few clouds"}],
+                },
+                {
+                    "dt_txt": "2022-01-03 12:00:00",
+                    "main": {"temp": 20.0},
+                    "weather": [{"description": "scattered clouds"}],
+                },
+                {
+                    "dt_txt": "2022-01-04 12:00:00",
+                    "main": {"temp": 25.0},
+                    "weather": [{"description": "broken clouds"}],
+                },
+                {
+                    "dt_txt": "2022-01-05 12:00:00",
+                    "main": {"temp": 30.0},
+                    "weather": [{"description": "overcast clouds"}],
+                },
             ]
         }
-        mock_response = unittest.mock.Mock()
-        mock_response.json.return_value = mock_data
-        mock_response.status_code = 200
-        mock_get.return_value = mock_response
-
-        expected_output = "Current weather in London: 20°C, sunny\n5-day forecast:\n2022-01-01 12:00:00: 18°C, partly cloudy\n2022-01-02 12:00:00: 16°C, cloudy\n"
-        self.assertEqual(get_weather("London"), expected_output)
-
-    @patch("weather.requests.get")
-    def test_get_weather_invalid_city(self, mock_get):
-        mock_response = unittest.mock.Mock()
-        mock_response.status_code = 404
-        mock_get.return_value = mock_response
-
-        expected_output = "No weather data found for InvalidCity\n"
-        self.assertEqual(get_weather("InvalidCity"), expected_output)
-
-    @patch("weather.requests.get")
-    def test_get_weather_request_error(self, mock_get):
-        mock_get.side_effect = requests.exceptions.RequestException("Test error")
-
-        expected_output = "An error occurred: Test error\n"
-        self.assertEqual(get_weather("London"), expected_output)
-
-    @patch("weather.requests.get")
-    def test_get_weather_http_error(self, mock_get):
-        mock_response = unittest.mock.Mock()
-        mock_response.status_code = 500
-        mock_get.return_value = mock_response
-
-        expected_output = "HTTP error occurred: 500 Server Error: Internal Server Error for url: https://api.openweathermap.org/data/2.5/forecast\n"
-        self.assertEqual(get_weather("London"), expected_output)
-
-    @patch("weather.requests.get")
-    def test_get_weather_parse_error(self, mock_get):
-        mock_response = unittest.mock.Mock()
-        mock_response.json.side_effect = ValueError("Test error")
-        mock_response.status_code = 200
-        mock_get.return_value = mock_response
-
-        expected_output = "Error parsing response: Test error\n"
-        self.assertEqual(get_weather("London"), expected_output)
+        get_weather("London")
+        mock_print.assert_called_with(
+            "\nCurrent weather in London:\n"
+            "┌─────────────┬────────────────┐\n"
+            "│ Temperature │ 10.0°C         │\n"
+            "├─────────────┼────────────────┤\n"
+            "│ Description │ clear sky      │\n"
+            "└─────────────┴────────────────┘\n"
+            "\n5-day forecast:\n"
+            "┌─────────────┬─────────────┬────────────────┐\n"
+            "│ Date        │ Temperature │ Description    │\n"
+            "├─────────────┼─────────────┼────────────────┤\n"
+            "│ 2022-01-02 12:00:00 │ 15.0°C         │ few clouds     │\n"
+            "│ 2022-01-03 12:00:00 │ 20.0°C         │ scattered clouds │\n"
+            "│ 2022-01-04 12:00:00 │ 25.0°C         │ broken clouds  │\n"
+            "│ 2022-01-05 12:00:00 │ 30.0°C         │ overcast clouds│\n"
+            "└─────────────┴─────────────┴────────────────┘\n"
+        )
 
 if __name__ == "__main__":
     unittest.main()
